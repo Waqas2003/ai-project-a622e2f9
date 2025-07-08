60379b32bafcc4f2833b1051fb9591fdd8fb3f65
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 20:44:46 +0500

    Add src\pages\HomePage.js

---
+++ src\pages\HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to our Ecommerce Website!</h1>
+      <p>Explore our products below:</p>
+      <ul>
+        <li><a href="/products">View All Products</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;
